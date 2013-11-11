628ee404746ecc845f93f88672f3e64f04045eb5
Ronald Cheng <rhc2104@columbia.edu>
2013-11-10 17:52:56 -0800

    Change url for sitemap

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -2,7 +2,7 @@ class SitemapsController < ApplicationController
 
   def show
     # Redirect to CloudFront and S3
-    redirect_to "https://d3pezy5dughvjp.cloudfront.net/sitemaps/sitemap1.xml.gz"
+    redirect_to "https://d3pezy5dughvjp.cloudfront.net/sitemaps/sitemap.xml.gz"
   end
 
 end
